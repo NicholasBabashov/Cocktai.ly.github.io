3c677fcea235820c50b71961419eac1b57f39ea2
Nichols Babashov <nicholasbabashov@gmail.com>
2023-10-11 18:59:56 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://docs.google.com/document/d/1zVr1SbByq11mrcQ_i__WMcCdwefm-jU8S56c3fTvHFs/edit#heading=h.vm051rmyhoww"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
