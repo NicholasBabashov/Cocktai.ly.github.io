27ad44c3db425f59e752c3ba4c2520fee022d520
Nicholas Babashov <nicholasbabashov@gmail.com>
2025-05-21 10:58:06 -0500

    Added button that adds stack that adds alcohol information

--- src/ProofCalculator.tsx
+++ src/ProofCalculator.tsx
@@ -1,17 +1,28 @@
-import { Paper, Typography, Stack, Box } from "@mui/material";
+import react, { useState } from "react";
+import { Paper, Typography, Stack, Box, Button } from "@mui/material";
+import { Ingredient } from "./Ingredient";
+import { Size } from "./Size";
+import { ABV } from "./ABV";
 
 export const ProofCalculator = () => {
+  const [componentsList, setComponentsList] = useState([{ id: Date.now() }]);
+
+  const addComponentRow = () => {
+  setComponentsList([...componentsList, { id: Date.now() }]);
+  };
+
   return (
     <Box>
       <Paper
         elevation={3}
         sx={{
           flexWrap: "wrap",
-          width: 512,
-          height: 512,
+          width: 400,
+          maxHeight: 512,
           margin: "auto",
           bgcolor: "#ece6f0",
           borderRadius: "15px",
+          overflowY: "auto"
         }}
       >
         <Typography
@@ -25,13 +36,28 @@ export const ProofCalculator = () => {
         >
           <br />
           Component Drinks
+          <Stack sx={{ padding: 2 }}>
+            {componentsList.map((item) => (
+              <Stack direction="row" spacing={1} key={item.id}>
+                <Ingredient />
+                <Size />
+                <ABV />
+              </Stack>
+            ))}
+          </Stack>
+
+          <Box textAlign="center">
+          <Button variant="contained" onClick={addComponentRow}>
+          +
+          </Button>
+</Box>
         </Typography>
       </Paper>
       <Paper
         elevation={3}
         sx={{
           flexWrap: "wrap",
-          width: 512,
+          width: 400,
           height: 256,
           margin: "auto",
           bgcolor: "#ece6f0",
